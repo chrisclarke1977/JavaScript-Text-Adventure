b608f0088c07b7c3bf766053a3a6498d6f7a2507
cclarkednata <50983656+cclarkednata@users.noreply.github.com>
2020-02-11 17:24:20 +0000

    Update game.js

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ function startGame() {
 }
 
 function showInv(inv) {
-  Object.keys(inv).forEach(i => document.querySelector("object").getSVGDocument().getElementById(i.toLowerCase()+'-icon').style.visibility = state[i] ? "visible" : "none" )
+  Object.keys(inv).forEach(i => document.querySelector("object").getSVGDocument().getElementById(i.toLowerCase()+'-icon').style.visibility = state[i] ? "visible" : "hidden" )
 }
 
 function showTextNode(textNodeIndex) {
